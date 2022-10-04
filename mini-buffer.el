c595b2f2167a5e8db393b7f90ff8d54e2b5c1a50
FoxCheeze <iagocarvalhodm@gmail.com>
2022-10-04 18:29:29 -0300

    refactor(trash): removed useless files

--- mini-buffer.el
+++
@@ -1,5 +0,0 @@
-;; Mini Buffer
-
-(setq history-length 25)
-(savehist-mode 1)
-
--- ui.el
+++
@@ -1,9 +0,0 @@
-;; Ui
-
-(setq inhibit-startup-message t)
-
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-
-
